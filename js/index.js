985b17e2c3ea6cc49f402dc4379ce51fbb4eefee
Xyotos <yuri.celentani@gmail.com>
2026-03-12 17:36:40 +0100

    TODO part 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
 const BACKEND_ROOT_URL = 'http://localhost:3001'
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
 
-const list = document.querySelector ('ul')
-const input = document.querySelector ('input')
+const renderTask = (task) => {
+    const li = document.createElement('li')
+    li.setAttribute('class', 'list-group-item')
+    li.innerHTML = task
+    list.append(li)
+}
 
-input.disabled = true
+const getTasks = async () => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL)
+        const json = await response.json()
+        json.forEach(task => {
+            renderTask(task.description)
+        })
+        input.disabled = false 
+    } catch (error) {
+        alert("Error retrieving tasks: " + error.message)
+    }
+}
+
+const saveTask = async (task) => {
+    try {
+        const json = JSON.stringify({ description: task })
+        const response = await fetch(BACKEND_ROOT_URL + '/new', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+        return response.json()
+    } catch (error) {
+        alert("Error saving task: " + error.message)
+    }
+}
 
 input.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
@@ -11,55 +44,12 @@ input.addEventListener('keypress', (event) => {
         const task = input.value.trim()
 
         if (task !== '') {
-            const li = document.createElement('li')
-            li.setAttribute('class', 'list-group-item')
-            li.innerHTML = task
-            list.append(li)
-            input.value = ''
+            saveTask(task).then((json) => {
+                renderTask(task)
+                input.value = ''
+            })
         }
     }
 })
-
-const renderTask = (task) => {
-    const li = document. createElement ('li')
-    li. setAttribute( 'class', 'list-group-item')
-    li. innerHTML = task
-    list. append (li)
-}
-input.addEventListener ('keypress', (event) => {
-    if (event. key === 'Enter') {
-        event.preventDefault()
-        const task = input. value. trim()
-        if (task != '') {
-            renderTask(task)
-            input.value = ''
-        }
-    }
-})
-
-const li = document.createElement('li')
-li.setAttribute('class','list-group-item')
-li.innerHTML = task
-list.append (li)
-
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL)
-        const json = await response.json()
-        json.forEach (task => {
-            renderTask(task.description)
-        })
-        input.disabled = false
-    } catch (error) {
-        alert("Error retrieving tasks " + error.message)
-    }
-}
-
-const saveTask = async (task) => {
-    saveTask(task).then((json)=> {
-        renderTask (task)
-        input. value = ''
-    })
-}
 
 getTasks()
