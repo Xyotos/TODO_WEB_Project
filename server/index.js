17afeeac5cb014dba5419f9f32bd177f1ece75e3
Xyotos <yuri.celentani@gmail.com>
2026-03-12 16:55:44 +0100

    todo part 1 & 2

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const cors = require('cors')
+const { Pool } = require('pg')
+
+const openDb = () => {
+    const pool = new Pool ({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: '1234',
+        port: 54
+    })
+    return pool
+}
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = 3001
+
+app.get("/",(req,res) => {
+    res.status(200).json({result: 'success'})
+})
+
+app.get("/", (req,res) => {
+    const pool = openDb()
+
+    pool.query('SELECT * FROM task', (error, result) => {
+        if( error ) {
+            res.status(500).json({ error: error.message })
+        }
+        res.status(200).json(result.rows)
+    })
+})
+
+app.post("/new",(req,res) => {
+    const pool = openDb()
+
+    pool.query('insert into task (description) values ($1) returning *',
+        [req.body.description],
+        (error,result) => {
+            if (error) {
+                res.status(500).json({error: error.message})
+            } else {
+                res.status(200).json({id : result.rows[0].id})
+            }
+        }
+    )
+})
+
+app.listen(port)
