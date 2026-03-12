6a7202e93bda4a0eb74cc698baf0fdf78d5f9f44
Xyotos <yuri.celentani@gmail.com>
2026-03-12 17:48:46 +0100

    TODO part 4

--- js/index.js
+++ js/index.js
@@ -1,41 +1,29 @@
 const BACKEND_ROOT_URL = 'http://localhost:3001'
+import { Todos } from "./class/Todos.js"
+
+const todos = new Todos(BACKEND_ROOT_URL)
+
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
+
+input.disabled = true
 
 const renderTask = (task) => {
     const li = document.createElement('li')
     li.setAttribute('class', 'list-group-item')
-    li.innerHTML = task
+    li.innerHTML = task.getText()
     list.append(li)
 }
 
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL)
-        const json = await response.json()
-        json.forEach(task => {
-            renderTask(task.description)
+const getTasks = () => {
+    todos.getTasks().then((tasks) => {
+        tasks.forEach(task => {
+            renderTask(task)
         })
-        input.disabled = false 
-    } catch (error) {
-        alert("Error retrieving tasks: " + error.message)
-    }
-}
-
-const saveTask = async (task) => {
-    try {
-        const json = JSON.stringify({ description: task })
-        const response = await fetch(BACKEND_ROOT_URL + '/new', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: json
-        })
-        return response.json()
-    } catch (error) {
-        alert("Error saving task: " + error.message)
-    }
+        input.disabled = false
+    }).catch((error) => {
+        alert(error)
+    })
 }
 
 input.addEventListener('keypress', (event) => {
@@ -44,9 +32,10 @@ input.addEventListener('keypress', (event) => {
         const task = input.value.trim()
 
         if (task !== '') {
-            saveTask(task).then((json) => {
+            todos.addTask(task).then((task) => {
                 renderTask(task)
                 input.value = ''
+                input.focus()
             })
         }
     }
