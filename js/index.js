ba44758fa7de2d56e678e8f86abc6f14c6e91a22
Xyotos <yuri.celentani@gmail.com>
2026-03-12 17:33:23 +0100

    Fix TODO part 1 & 2

--- js/index.js
+++ js/index.js
@@ -1,16 +1,65 @@
-const list = document.querySelector('ul')
-const input = document.querySelector('input')
+const BACKEND_ROOT_URL = 'http://localhost:3001'
 
-input.addEventListener('keypress',(event) => {
-    if(event.key === 'Enter') {
+const list = document.querySelector ('ul')
+const input = document.querySelector ('input')
+
+input.disabled = true
+
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
         event.preventDefault()
         const task = input.value.trim()
-        if(task != '') {
+
+        if (task !== '') {
             const li = document.createElement('li')
-            li.setAttribute('class','list-group-item')
+            li.setAttribute('class', 'list-group-item')
             li.innerHTML = task
             list.append(li)
             input.value = ''
         }
     }
 })
+
+const renderTask = (task) => {
+    const li = document. createElement ('li')
+    li. setAttribute( 'class', 'list-group-item')
+    li. innerHTML = task
+    list. append (li)
+}
+input.addEventListener ('keypress', (event) => {
+    if (event. key === 'Enter') {
+        event.preventDefault()
+        const task = input. value. trim()
+        if (task != '') {
+            renderTask(task)
+            input.value = ''
+        }
+    }
+})
+
+const li = document.createElement('li')
+li.setAttribute('class','list-group-item')
+li.innerHTML = task
+list.append (li)
+
+const getTasks = async () => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL)
+        const json = await response.json()
+        json.forEach (task => {
+            renderTask(task.description)
+        })
+        input.disabled = false
+    } catch (error) {
+        alert("Error retrieving tasks " + error.message)
+    }
+}
+
+const saveTask = async (task) => {
+    saveTask(task).then((json)=> {
+        renderTask (task)
+        input. value = ''
+    })
+}
+
+getTasks()
