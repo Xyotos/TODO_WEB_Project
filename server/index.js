8224ea34bb94611abcb3627be64253f650ccb0cd
Xyotos <yuri.celentani@gmail.com>
2026-03-17 11:42:25 +0100

    TODO part 7 + Fix to gitignore

--- server/index.js
+++ server/index.js
@@ -1,57 +1,16 @@
 require('dotenv').config()
-console.log(process.env)
 const express = require('express')
 const cors = require('cors')
-//const { Pool } = require('pg')
-const { query } = require('./helpers/db.js')
+const { todoRouter } = require('./routes/todo.js')
 
 const app = express()
 app.use(cors())
-app.use(express.json()) 
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const port = process.env.PORT
+app.use('/', todoRouter)
 
-const openDb = () => {
-    const pool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
-    })
-    return pool
-};
-
-app.get("/", async (req, res) => {
-    try {
-        const result = await query('select * from task')
-        const rows = result.rows ? result.rows : []
-        res.status(200).json(rows)
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-})
-
-app.post("/new", async (req, res) => {
-    try {
-        const result = await query('insert into task (description) values ($1) returning *', 
-            [req.body.description])
-        res.status(200).json({ id: result.rows[0].id })
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-})
-
-app.delete("/delete/:id", async (req, res) => {
-    try {
-        const id = Number(req.params.id)
-        await query('delete from task where id = $1', [id])
-        res.status(200).json({ id: id })
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-})
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
