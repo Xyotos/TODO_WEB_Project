101c80526c508fee386e890dd381a25fecd50d19
Xyotos <yuri.celentani@gmail.com>
2026-03-17 11:14:01 +0100

    TODO part 5

--- server/index.js
+++ server/index.js
@@ -5,18 +5,19 @@ const { Pool } = require('pg')
 const app = express()
 app.use(cors())
 app.use(express.json()) 
+app.use(express.urlencoded({ extended: false }))
+
 const port = 3001
 
 const openDb = () => {
-    const pool = new Pool({
+    return new Pool({
         user: 'postgres',
         host: 'localhost',
         database: 'todo',
         password: '1234',
         port: 5432
-    })
-    return pool
-}
+    });
+};
 
 app.get("/", (req, res) => {
     const pool = openDb()
@@ -42,6 +43,16 @@ app.post("/new", (req, res) => {
     })
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+app.delete("/delete/:id", async (req, res) => {
+    const pool = openDb()
+    const id = parseInt(req.params.id)
+    pool.query('DELETE FROM task WHERE id = $1', [id], (error, result) => {
+        if (error){
+            res.status(500).json({ error: error.message })
+        } else {
+            res.status(200).json({ id: id })
+        }
+    })
 })
+
+app.listen(port);
