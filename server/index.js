c70279c4778320ac61cf30ac051ea42b4c71053a
Xyotos <yuri.celentani@gmail.com>
2026-03-17 11:23:01 +0100

    TODO part 6

--- server/index.js
+++ server/index.js
@@ -1,58 +1,58 @@
+require('dotenv').config()
+console.log(process.env)
 const express = require('express')
 const cors = require('cors')
-const { Pool } = require('pg')
+//const { Pool } = require('pg')
+const { query } = require('./helpers/db.js')
 
 const app = express()
 app.use(cors())
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: false }))
 
-const port = 3001
+const port = process.env.PORT
 
 const openDb = () => {
-    return new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'todo',
-        password: '1234',
-        port: 5432
-    });
+    const pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    })
+    return pool
 };
 
-app.get("/", (req, res) => {
-    const pool = openDb()
-    pool.query('SELECT * FROM task', (error, result) => {
-        if (error) {
-            res.status(500).json({ error: error.message })
-        } else {
-            res.status(200).json(result.rows)
-        }
-    })
+app.get("/", async (req, res) => {
+    try {
+        const result = await query('select * from task')
+        const rows = result.rows ? result.rows : []
+        res.status(200).json(rows)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 })
 
-app.post("/new", (req, res) => {
-    const pool = openDb()
-    pool.query('insert into task (description) values ($1) returning *', 
-    [req.body.description], 
-    (error, result) => {
-        if (error) {
-            res.status(500).json({ error: error.message })
-        } else {
-            res.status(200).json({ id: result.rows[0].id })
-        }
-    })
+app.post("/new", async (req, res) => {
+    try {
+        const result = await query('insert into task (description) values ($1) returning *', 
+            [req.body.description])
+        res.status(200).json({ id: result.rows[0].id })
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 })
 
 app.delete("/delete/:id", async (req, res) => {
-    const pool = openDb()
-    const id = parseInt(req.params.id)
-    pool.query('DELETE FROM task WHERE id = $1', [id], (error, result) => {
-        if (error){
-            res.status(500).json({ error: error.message })
-        } else {
-            res.status(200).json({ id: id })
-        }
-    })
+    try {
+        const id = Number(req.params.id)
+        await query('delete from task where id = $1', [id])
+        res.status(200).json({ id: id })
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 })
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
